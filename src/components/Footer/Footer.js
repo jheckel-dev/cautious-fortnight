a7e45258a3720037228625fef4a705402616ef41
jheckel <jalen.heckel@icloud.com>
2021-11-23 12:47:45 -0800

    updated portfolio to add additional projects

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/https://jheckel-dev.github.io/cautious-fortnight/'
+      className='link footer__link'
+    >
+      Created By Jalen Heckel
+    </a>
+  </footer>
+)
+
+export default Footer
--- src/portfolio.js
+++ src/portfolio.js
@@ -16,14 +16,14 @@ const about = {
 }
 
 const projects = [
-  // projects can be added an removed
+  
   {
-    name: 'Halloween RSVP',
+    name: 'Day Planner',
     description:
-      'An application meant to be an app where invitees to a party can share their costume, a snack they are bringing, and make a song request for a halloween party.',
-    stack: ['HandleBars', 'SQL', ''],
-    sourceCode: 'https://github.com/jheckel-dev/halloween-rsvp',
-    Picture: 'src/img/Screen Shot 2021-10-01 at 10.03.15 AM.png',
+      'An application that allows the user to save tasks for each hour of the work day.',
+    stack: ['momentjs', 'JavaScript', ''],
+    sourceCode: 'https://github.com/jheckel-dev/daily-planner',
+    livePreview: 'https://jheckel-dev.github.io/daily-planner/',
   },
   {
     name: 'Forget Me Nots',
@@ -31,15 +31,42 @@ const projects = [
       'A simple note taking application using express.js to persistently store notes on local storage.',
     stack: ['JavaScript', 'Express.js', ''],
     sourceCode: 'https://https://github.com/jheckel-dev/forget-me-nots.com',
-    livePreview: 'https://ghttps://github.com/jheckel-dev/forget-me-nots/blob/main/Assets/ezgif-6-9337a735ed12.gifithub.com',
+    livePreview: 'https://note-taker-shmote-shmaker.herokuapp.com',
   },
   {
     name: 'E-Commerce Back End',
     description:
       'The backend for an online shopping retailer where items can be added, removed, and updated using JSON input form the user.',
-    stack: ['SQL', 'JavaScript', 'API'],
+    stack: ['SQL', 'JavaScript', 'Expressjs'],
     sourceCode: 'https://https://github.com/jheckel-dev/E-Commerce-Back-End.com',
     livePreview: 'https://githttps://github.com/jheckel-dev/E-Commerce-Back-End/blob/main/Screen-Recording-2021-11-18-at-3.04.50-PM.gifhub.com',
+  },
+  {
+    // 
+    name: 'Password Generator',
+    description:
+      'A simple JavaScript application that generats a random password for users to use on another site.',
+    stack: ['JavaScript', 'HTML', 'CSS'],
+    sourceCode: 'https://github.com/jheckel-dev/password-generator',
+    livePreview: 'https://jheckel-dev.github.io/password-generator/'
+  },
+  {
+    // 
+    name: 'Code Quiz!',
+    description:
+      'A Basic coding uiz with a timer and a method to track the users score.',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    sourceCode: 'https://github.com/jheckel-dev/code-quiz',
+    livePreview: 'https://jheckel-dev.github.io/code-quiz/',
+  },
+  {
+    // 
+    name: 'Signs and Events',
+    description:
+      'An application where we take in the users DOB and uses third party APIs to return some world events and astrological information about the user.',
+    stack: ['SQL', 'JavaScript', 'API'],
+    sourceCode: 'https://github.com/jheckel-dev/signs-and-events',
+    livePreview: 'https://jheckel-dev.github.io/signs-and-events/',
   },
 ]
 
@@ -61,7 +88,7 @@ const skills = [
 ]
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
+ 
   email: 'jalen.heckel@icloud.com',
 }
 
